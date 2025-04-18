67f4cc99828f89a3c8c59eaef081f002bf45d2ee
Theo985 <64311621+Theo985@users.noreply.github.com>
2025-04-18 02:29:24 +0200

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,8 +1,16 @@
--- ID du jeu que tu veux détecter
 local expectedGameId = 72829404259339
 
--- Vérifie si l'ID du jeu actuel correspond à l'ID attendu
-if game.PlaceId ~= expectedGameId then
-    -- Si l'ID ne correspond pas, on kick le joueur
-    game.Players.LocalPlayer:Kick("Vous n'êtes pas dans le bon jeu !")
+if game.PlaceId == expectedGameId then
+    local screenGui = Instance.new("ScreenGui")
+    screenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+    
+    local panel = Instance.new("Frame")
+    panel.Parent = screenGui
+    panel.Size = UDim2.new(0, 400, 0, 200)  -- Taille du panneau (400x200)
+    panel.Position = UDim2.new(0.5, -200, 0.5, -100)  
+    panel.BackgroundColor3 = Color3.fromRGB(0, 0, 0)  
+    panel.BackgroundTransparency = 0.993  
+    panel.BorderRadius = UDim.new(0, 20) 
+    panel.Draggable = true 
+    panel.Active = true  
 end
